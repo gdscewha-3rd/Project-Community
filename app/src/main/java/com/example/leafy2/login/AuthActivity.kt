09182f1456f048830d163b9946ea9ec870985ad5
EmilyCh0 <67777523+EmilyCh0@users.noreply.github.com>
2021-12-31 21:52:30 +0900

    로그인 화면 구성

---
+++ app/src/main/java/com/example/leafy2/login/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.leafy2.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.leafy2.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
