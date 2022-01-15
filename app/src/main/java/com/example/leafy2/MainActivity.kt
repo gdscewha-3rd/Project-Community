3e030f73d5b6992da7b2c2d385eb0eda8de3102a
EmilyCh0 <67777523+EmilyCh0@users.noreply.github.com>
2022-01-15 11:12:22 +0900

    촬영한 사진 화면에 띄우기

--- app/src/main/java/com/example/leafy2/MainActivity.kt
+++ app/src/main/java/com/example/leafy2/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.leafy2
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputBinding
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.leafy2.databinding.ActivityMainBinding
 import com.example.leafy2.login.AuthActivity
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
--- app/src/main/java/com/example/leafy2/diagnosis/DiagnosisFragment.kt
+++ app/src/main/java/com/example/leafy2/diagnosis/DiagnosisFragment.kt
@@ -1,19 +1,41 @@
 package com.example.leafy2.diagnosis
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.databinding.tool.util.FileUtil
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.os.FileUtils
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.tool.util.FileUtil
 import com.example.leafy2.R
 import com.example.leafy2.databinding.FragmentDiagnosisBinding
 
 
 class DiagnosisFragment : Fragment() {
 
-    private var _binding: FragmentDiagnosisBinding? = null
-    private val binding get() = _binding!!
+    val CAMERA = arrayOf(android.Manifest.permission.CAMERA)
+    val CAMERA_REQUEST = 98
 
+    private lateinit var binding: FragmentDiagnosisBinding
+
+    private lateinit var resultTv: TextView
+    private lateinit var image: ImageView
+    private lateinit var labels: List<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +46,66 @@ class DiagnosisFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentDiagnosisBinding.inflate(inflater, container, false)
+        binding = FragmentDiagnosisBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        resultTv = binding.resultTv
+        binding.captureIv.setOnClickListener { takePhoto() }
+        image = binding.captureIv
+    }
 
+    private fun takePhoto(){
+        if(checkPermission(CAMERA)){
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, CAMERA_REQUEST)
+        }
+    }
 
+    private fun checkPermission(permissions: Array<out String>): Boolean{
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            for( permission in permissions){
+                if(context?.let { ContextCompat.checkSelfPermission(it, permission) } != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(context as Activity, permissions, CAMERA_REQUEST)
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            CAMERA_REQUEST -> {
+                for(grant in grantResults){
+                    if(grant != PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(context, "카메라 권한을 허용해주세요", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode==RESULT_OK){
+            when (requestCode){
+                CAMERA_REQUEST -> {
+                    val bitmap = data?.extras?.get("data") as Bitmap
+                    image?.setImageBitmap(bitmap)
+                }
+            }
+        }
+    }
+
+    fun classifyImage(){
+        labels = FileUtil.load
+    }
 }
